f492a00a29cdbcadbe2134a8ca7a09e5cbc54eca
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2025-02-23 08:47:06 +0000

    コンテストで、問題ページを自動生成

--- web/html/js/submit.js
+++ web/html/js/submit.js
@@ -1,6 +1,8 @@
-const editor = ace.edit("editor", {
-    fontSize: "15px"
-})
+if (document.querySelector("#editor")!=undefined) {
+    const editor = ace.edit("editor", {
+        fontSize: "15px"
+    })
+}
 
 async function getData(data) {
     const url = "https://api.maicoder.f5.si/judge/test";
@@ -17,6 +19,12 @@ async function getData(data) {
     } catch (error) {
         return error
     }
+}
+
+async function get_problem(id) {
+    const response = await fetch("https://api.maicoder.f5.si/problems/" +id)
+    const json = await response.json()
+    return await json
 }
 
 async function go() {
@@ -42,11 +50,13 @@ async function go() {
 }
 
 function compiler_change() {
-    let compiler = document.querySelector("select.compiler").value.split(",")[0]
-    editor.session.setMode(`ace/mode/${compiler}`);
+    if (document.querySelector("select.compiler")) {
+        let compiler = document.querySelector("select.compiler").value.split(",")[0]
+        editor.session.setMode(`ace/mode/${compiler}`);
+    }
 }
 
-function badge(state){
+function badge(state) {
     return `<div class="badge ${state}"></div>`
 }
 
