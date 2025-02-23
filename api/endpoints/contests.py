36bfed789bbd61e98b3fe789d60714dd53a05124
launchpencil <launchpencil@gmail.com>
2025-02-23 11:40:46 +0900

    一旦実装

--- api/endpoints/contests.py
+++ api/endpoints/contests.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 import os
 import json
+from datetime import datetime
 
 router = APIRouter()
 problems_dir = "/app/data/contests/"
@@ -10,6 +11,9 @@ async def get_contest_datail(subpath: str):
     try:
         with open(f"{problems_dir}{subpath}.json", "r") as file:
             content = json.load(file)
+            contest_date = datetime.fromisoformat(content.get("date"))
+            if contest_date < datetime.now():
+                del content["problems"]
         return content
     except FileNotFoundError:
         return {"error": "File not found"}, 404
