56d9ed1cd6c3c0f75a0aca01f599da783ac8e1c6
launchpencil <launchpencil@gmail.com>
2025-03-23 03:09:20 +0900

    API

--- api/endpoints/submissions.py
+++ api/endpoints/submissions.py
@@ -3,3 +3,18 @@ import os
 import json
 
 router = APIRouter()
+
+submit_dir = "/app/data/submissions/"
+
+@router.get("/{subpath:path}")
+async def get_submissions(subpath: str):
+    try:
+        with open(f"{submit_dir}{subpath}.json", "r") as file:
+            content = json.load(file)
+        return content
+    except FileNotFoundError:
+        return {"error": "File not found"}, 404
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON format"}, 400
+    except Exception as e:
+        return {"error": str(e)}, 500
