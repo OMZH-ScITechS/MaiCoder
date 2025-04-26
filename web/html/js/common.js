27bb66c4fc483b865b9e535d9a10286b49af0dd3
pcsuke <sukepc0824@gmail.com>
2025-04-27 08:51:18 +0900

    ファイル構造を変更

---
+++ web/html/js/common.js
@@ -0,0 +1,64 @@
+let editor;
+if (document.querySelector("#editor")!=undefined) {
+    editor = ace.edit("editor", {
+        fontSize: "15px",
+    })
+}
+
+async function get_judge(data) {
+    const url = "https://api.maicoder.f5.si/judge/test";
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`レスポンスステータス: ${response.status}`);
+        }
+        const json = await response.json();
+        return json
+    } catch (error) {
+        return error
+    }
+}
+
+async function get_problem(id) {
+    const response = await fetch("https://api.maicoder.f5.si/problems/" +id)
+    const json = await response.json()
+    return await json
+}
+
+async function go() {
+    let data = {
+        compiler: document.querySelector("select.compiler").value.split(",")[1],
+        code: editor.getValue(),
+        stdin: document.querySelector("textarea.input").value
+    }
+
+    console.log(data)
+
+    let $button = document.querySelector("button.go")
+    $button.classList.add("loading")
+
+    const response = await get_judge(data)
+
+    let $output = document.querySelector(".output code")
+    let $speed = document.querySelector("span.speed")
+
+    $button.classList.remove("loading")
+    $output.innerText = response.message.program_message
+    $speed.innerText = Math.round(response.speed * 1000) + ' ms'
+}
+
+function compiler_change() {
+    if (document.querySelector("select.compiler")) {
+        let compiler = document.querySelector("select.compiler").value.split(",")[0]
+        editor.session.setMode(`ace/mode/${compiler}`);
+    }
+}
+
+function badge(state) {
+    return `<div class="badge ${state}"></div>`
+}
+
+compiler_change()
